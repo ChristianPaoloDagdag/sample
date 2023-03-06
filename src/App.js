6b457ebf1786a36b149a39a2a0be813613c7cc30
ChristianPaoloDagdag <christianpaolodagdag@gmail.com>
2023-03-06 12:29:26 +0800

    remove dagdag from header

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Paolo Dagdag Information</div>;
+  return <div className="App">Paolo Information</div>;
 }
 
 export default App;
